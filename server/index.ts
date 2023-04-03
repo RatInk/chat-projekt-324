55d52750ce3ba03539ea171ddbeb7dfd726ee0e4
ryad600 <84077602+ryad600@users.noreply.github.com>
2023-04-03 15:59:14 +0200

    added the feature to get time when the endpoint is used

--- server/index.ts
+++ server/index.ts
@@ -31,6 +31,10 @@ app.use(express.static('client'));
 app.get('/', (req: Request, res: Response) => {
   res.sendFile(__dirname + '/client/index.html');
 });
+app.get('/log/time', (req: Request, res: Response) => {
+  const currentTime = new Date().toLocaleTimeString();
+  res.send("The current time is " + currentTime + ".");
+});
 // Initialize the websocket server
 initializeWebsocketServer(server);
 
